0367b19f1fd951854757a4b4ba43a30f4b5e2728
Kejia Wu <kejiawu@sharethis.com>
2023-12-04 21:11:08 -0600

    feat: Remove redundent code.

--- app/src/scripts/setup.sql
+++ app/src/scripts/setup.sql
@@ -7,37 +7,10 @@ create application role if not exists app_instance_role;
 create or alter versioned schema app_instance_schema;
 
 -- Share data
-create or replace view app_instance_schema.MFG_SHIPPING as select * from shared_content_schema.MFG_SHIPPING;
 create or replace view app_instance_schema.SHARETHIS_DATA as select * from shared_content_schema.SHARETHIS_DATA;
 
 -- Create Streamlit app
 create or replace streamlit app_instance_schema.streamlit from '/libraries' main_file='streamlit.py';
-
--- Create UDFs
-create or replace function app_instance_schema.cal_lead_time(i int, j int, k int)
-returns float
-language python
-runtime_version = '3.8'
-packages = ('snowflake-snowpark-python')
-imports = ('/libraries/udf.py')
-handler = 'udf.cal_lead_time';
-
-create or replace function app_instance_schema.cal_distance(slat float,slon float,elat float,elon float)
-returns float
-language python
-runtime_version = '3.8'
-packages = ('snowflake-snowpark-python','pandas','scikit-learn==1.1.1')
-imports = ('/libraries/udf.py')
-handler = 'udf.cal_distance';
-
--- Create Stored Procedure
-create or replace procedure app_instance_schema.billing_event(number_of_rows int)
-returns string
-language python
-runtime_version = '3.8'
-packages = ('snowflake-snowpark-python')
-imports = ('/libraries/procs.py')
-handler = 'procs.billing_event';
 
 create or replace procedure app_instance_schema.update_reference(ref_name string, operation string, ref_or_alias string)
 returns string
@@ -58,23 +31,9 @@ begin
 end;
 $$;
 
-create or replace function app_instance_schema.hello_world()
-returns string
-language python
-runtime_version = '3.8'
-packages = ('snowflake-snowpark-python')
-imports = ('/libraries/udf.py')
-handler = 'udf.hello_world';
-
 -- Grant usage and permissions on objects
 grant usage on schema app_instance_schema to application role app_instance_role;
-grant usage on function app_instance_schema.cal_lead_time(int,int,int) to application role app_instance_role;
-grant usage on procedure app_instance_schema.billing_event(int) to application role app_instance_role;
-grant usage on function app_instance_schema.cal_distance(float,float,float,float) to application role app_instance_role;
-grant SELECT on view app_instance_schema.MFG_SHIPPING to application role app_instance_role;
 grant SELECT on view app_instance_schema.SHARETHIS_DATA to application role app_instance_role;
 grant usage on streamlit app_instance_schema.streamlit to application role app_instance_role;
 grant usage on procedure app_instance_schema.update_reference(string, string, string) to application role app_instance_role;
-
-grant usage on function app_instance_schema.hello_world() to application role app_instance_role;
 
